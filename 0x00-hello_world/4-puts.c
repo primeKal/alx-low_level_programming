d24b2d5f5a7d6afbf52b922dd10bf0a478d6b4de
kaleb <kalebteshale72@gmail.com>
2023-06-15 12:05:43 -0700

    hi

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Main is the entry point
+ * We will return zero here
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
