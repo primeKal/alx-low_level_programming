a9de195729cd1f4f78d70530be298ef34b2fff87
kaleb <kalebteshale72@gmail.com>
2023-06-29 05:37:43 -0700

    ji

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints 3 combinations of digits
- * Retunr: 0
+ * Return: 0
  */
 int main(void)
 {
@@ -12,7 +12,7 @@ int main(void)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for(l = 50; l < 58; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
