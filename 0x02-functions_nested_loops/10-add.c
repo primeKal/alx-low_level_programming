e9cc8df9062e36d4bbcdf22659c2465d6fc52dd6
kaleb <kalebteshale72@gmail.com>
2023-06-30 01:13:21 -0700

    hi

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add 2 int
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints 50 fibonacci numbers
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+	unsigned long int sum, j, k, next;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+			sum = sum + j;
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	
+	printf("%lu\n", sum);
+
+	return (0);
+}
