c69b0ad03c3e4bc37e0ba1e6906d9b3c9a1603e8
kaleb <kalebteshale72@gmail.com>
2023-07-18 00:35:27 -0700

    0x0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+
+/**
+ * main - prints the name of file
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%S\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is a flder containting answers for preprocessor question. mostly header files
